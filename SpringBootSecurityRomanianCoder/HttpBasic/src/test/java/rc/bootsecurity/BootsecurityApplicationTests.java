cdceca55026d185dc6f0773d84e5ff5c0a97b8c7
Monir Ahmed <monir@newroztech.com>
2023-03-22 21:19:46 +0600

    feat: http basic authentication applied

---
+++ SpringBootSecurityRomanianCoder/HttpBasic/src/test/java/rc/bootsecurity/BootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package rc.bootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
