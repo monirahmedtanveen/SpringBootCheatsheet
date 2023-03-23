666c1476c3bccbfe57ac1c9464e8cb196e9023c4
Monir Ahmed <monir@newroztech.com>
2023-03-23 18:45:45 +0600

    feat: custom http basic authentication

--- SpringBootSecurityRomanianCoder/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ SpringBootSecurityRomanianCoder/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -17,16 +17,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("monir.ahmed").password(passwordEncoder().encode("Password100@")).roles("USER");
+                .withUser("monir.ahmed")
+                .password(passwordEncoder().encode("Password100@"))
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
     }
